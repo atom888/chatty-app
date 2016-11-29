562d8da7c80fcfc73aa0bf10f6475a056c35954d
Adam <adam.dipinto@gmail.com>
2016-11-29 02:17:38 +0000

    Completed w5d1 exercises. Created CharBar.jsx to container footer for the page, MessageList to contain the Message.jsx and update App to import new components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
+    console.log("Rendering")
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/Message.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+
+class Message extends Component {
+  render() {
+    return (
+      <div>
+        <div className="message">
+          <span className="username">
+            Anonymous1:
+          </span>
+          <span className="content">
+            I won't be impressed with technology until I can download food.
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+export default Message;
+
+
+
+
+
+
